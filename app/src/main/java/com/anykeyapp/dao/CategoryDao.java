4719aaed4144fb62c35bbc908834ccf2ad722f79
vvoloshyn <vladvoloshyn12@gmail.com>
2017-05-13 23:24:57 +0300

    dao models add

--- app/src/main/java/com/anykeyapp/dao/CategoryDao.java
+++ app/src/main/java/com/anykeyapp/dao/CategoryDao.java
@@ -1,5 +1,28 @@
 package com.anykeyapp.dao;
 
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
+import com.anykeyapp.dao.models.Category;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.util.List;
+
 public class CategoryDao {
 
+    public void create(Category category) {
+        category.save();
+    }
+
+    public List<Category> read() {
+        return Stream.of(SQLite.select().from(Category.class).queryList())
+                .collect(Collectors.toList());
+    }
+
+    public void update(Category category) {
+        create(category);
+    }
+
+    public void delete(Category category) {
+        category.delete();
+    }
 }
--- app/src/main/java/com/anykeyapp/dao/ProductDao.java
+++ app/src/main/java/com/anykeyapp/dao/ProductDao.java
@@ -1,4 +1,28 @@
 package com.anykeyapp.dao;
 
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
+import com.anykeyapp.dao.models.ProductItem;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.util.List;
+
 public class ProductDao {
+
+    public void create(ProductItem product) {
+        product.save();
+    }
+
+    public List<ProductItem> read() {
+        return Stream.of(SQLite.select().from(ProductItem.class).queryList())
+                .collect(Collectors.toList());
+    }
+
+    public void update(ProductItem product) {
+        create(product);
+    }
+
+    public void delete(ProductItem product) {
+        product.delete();
+    }
 }
