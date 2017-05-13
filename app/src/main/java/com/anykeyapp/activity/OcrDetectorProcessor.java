dadebabc9cd7a8b34a68c083ef3ff85358849e6a
vvoloshyn <vladvoloshyn12@gmail.com>
2017-05-13 15:51:19 +0300

    textRecognition

---
+++ app/src/main/java/com/anykeyapp/activity/OcrDetectorProcessor.java
@@ -0,0 +1,46 @@
+package com.anykeyapp.activity;
+
+import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.android.gms.samples.vision.ocrreader.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+
+import com.anykeyapp.activity.google_vision.camera.*;
+
+/**
+ * A very simple Processor which gets detected TextBlocks and adds them to the overlay
+ * as OcrGraphics.
+ * TODO: Make this implement Detector.Processor<TextBlock> and add text to the GraphicOverlay
+ */
+public class OcrDetectorProcessor implements Detector.Processor<TextBlock> {
+
+    private GraphicOverlay<OcrGraphic> mGraphicOverlay;
+
+    OcrDetectorProcessor(GraphicOverlay<OcrGraphic> ocrGraphicOverlay) {
+        mGraphicOverlay = ocrGraphicOverlay;
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections<TextBlock> detections) {
+        mGraphicOverlay.clear();
+        SparseArray<TextBlock> items = detections.getDetectedItems();
+        for (int i = 0; i < items.size(); ++i) {
+            TextBlock item = items.valueAt(i);
+            if (item != null && item.getValue() != null) {
+                Log.d("Processor", "Text detected! " + item.getValue());
+            }
+            OcrGraphic graphic = new OcrGraphic(mGraphicOverlay, item);
+            mGraphicOverlay.add(graphic);
+        }
+    }
+
+    @Override
+    public void release() {
+        mGraphicOverlay.clear();
+    }
+
+
+    // TODO:  Once this implements Detector.Processor<TextBlock>, implement the abstract methods.
+}
