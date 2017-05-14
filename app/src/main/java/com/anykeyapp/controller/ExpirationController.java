9d06ef209f04963db9a8b1904d0fc1cf76727d0e
olexandr <kutovio@gmail.com>
2017-05-14 06:38:07 +0300

    Add notification

--- app/src/main/java/com/anykeyapp/controller/ExpirationController.java
+++ app/src/main/java/com/anykeyapp/controller/ExpirationController.java
@@ -1,8 +1,14 @@
 package com.anykeyapp.controller;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
+import com.anykeyapp.R;
+import com.anykeyapp.activity.MainActivity;
 import com.anykeyapp.dao.ProductDao;
 import com.anykeyapp.dao.models.ProductItem;
 
@@ -10,6 +16,8 @@ import java.util.Date;
 import java.util.List;
 
 import rx.subscriptions.CompositeSubscription;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class ExpirationController {
 
@@ -77,6 +85,27 @@ public class ExpirationController {
             product.freshStatus = true;
 
         productDao.update(product);
+
+        if (!product.freshStatus) {
+            showNotification(product);
+        }
+    }
+
+    private void showNotification(ProductItem productItem) {
+//      // Prepare intent which is triggered if the
+        // notification is selected
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
+
+        // Build notification
+        // Actions are just fake
+        Notification noti = new Notification.Builder(context)
+                .setContentTitle("Your " + productItem.name + " has expired")
+                .setContentText("Subject").setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentIntent(pIntent).build();
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        // hide the notification after its selected
+        notificationManager.notify(0, noti);
     }
 
     public void wakeUp() {
