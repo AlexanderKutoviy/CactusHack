f9603668de0afad4b19df434338b50846f727eab
olexandr <kutovio@gmail.com>
2017-05-14 06:45:20 +0300

    Add location tracker

--- app/src/main/java/com/anykeyapp/controller/LocationReceiver.java
+++ app/src/main/java/com/anykeyapp/controller/LocationReceiver.java
@@ -13,9 +13,8 @@ public class LocationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (null != intent && intent.getAction().equals("my.action")) {
-            Location locationData = (Location) intent.getParcelableExtra(SettingsLocationTracker.LOCATION_MESSAGE);
-            Log.d("Location: ", "Latitude: " + locationData.getLatitude() + "Longitude:" + locationData.getLongitude());
-            //send your call to api or do any things with the of location data
+            Location locationData = intent.getParcelableExtra(SettingsLocationTracker.LOCATION_MESSAGE);
+            Log.e("Location: ", "Latitude: " + locationData.getLatitude() + "Longitude:" + locationData.getLongitude());
         }
     }
 }
